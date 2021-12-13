6c35c1b1bc639ea54a16b853c6022b77bc946716
Hendrik Geiger <g9a2hvk9@gmail.com>
2021-12-13 10:33:02 +0100

    adds static route pointing to frontend build directory to serve react index file

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
