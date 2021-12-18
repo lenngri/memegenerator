e523ec103fd6e148db3990fce16bc7e7fde8a82c
Hendrik Geiger <g9a2hvk9@gmail.com>
2021-12-18 15:11:00 +0100

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch('/users')
+  .then(response => response.text())
+  .then(data => console.log({data}));
+
   return (
     <div className="App">
       <header className="App-header">
