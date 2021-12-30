5488e803b4d39c157fc0e35eac90985ca9cbabc3
TiWgnr <82907190+TiWgnr@users.noreply.github.com>
2021-12-30 19:00:02 +0100

    Tried to set input in textfield equal to speech result

--- frontend/src/components/VoiceToText.js
+++ frontend/src/components/VoiceToText.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../components/VoiceToText.css";
+import TextField from "@mui/material/TextField";
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -13,8 +14,41 @@ function VoiceToText() {
   const [isListening, setIsListening] = useState(false);
   const [note, setNote] = useState(null);
   const [savedNotes, setSavedNotes] = useState([]);
+  const [name, setName] = React.useState();
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
   useEffect(() => {
+    const handleListen = () => {
+      if (isListening) {
+        mic.start();
+        mic.onend = () => {
+          console.log("continue..");
+          mic.start();
+        };
+      } else {
+        mic.stop();
+        mic.onend = () => {
+          console.log("Stopped Mic on Click");
+        };
+      }
+      mic.onstart = () => {
+        console.log("Mics on");
+      };
+
+      mic.onresult = (event) => {
+        const transcript = Array.from(event.results)
+          .map((result) => result[0])
+          .map((result) => result.transcript)
+          .join("");
+        console.log(transcript);
+        setNote(transcript);
+        mic.onerror = (event) => {
+          console.log(event.error);
+        };
+      };
+    };
     handleListen();
   }, [isListening]);
 
@@ -53,27 +87,82 @@ function VoiceToText() {
     setNote("");
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log(event.currentTarget);
+  };
+
   return (
     <>
-      <h1>Voice Notes</h1>
+      <h1>Voice to Text</h1>
       <div className="container">
         <div className="box">
-          <h2>Current Note</h2>
+          <h2>Current Text</h2>
           {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
           <button onClick={handleSaveNote} disabled={!note}>
-            Save Note
+            Save as Title
           </button>
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save as Caption 1
+          </button>
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save as Caption 2
+          </button>
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save as Caption 3
+          </button>
+
           <button onClick={() => setIsListening((prevState) => !prevState)}>
             Start/Stop
           </button>
           <p>{note}</p>
         </div>
-        <div className="box">
-          <h2>Notes</h2>
-          {savedNotes.map((n) => (
-            <p key={n}>{n}</p>
-          ))}
-        </div>
+      </div>
+      <div style={{ paddingLeft: 50 }}>
+        <TextField
+          id="outlined-basic"
+          label={note ? "" : "Title"}
+          variant="outlined"
+          value={name ? name : note}
+          onChange={handleChange}
+          onClick={() => setIsListening((prevState) => !prevState)}
+        />
+      </div>
+      <div style={{ paddingLeft: 50 }}>
+        <TextField id="outlined-basic" label="Caption 1" variant="outlined" />
+      </div>
+      <div style={{ paddingLeft: 50 }}>
+        <TextField id="outlined-basic" label="Caption 2" variant="outlined" />
+      </div>
+      <div style={{ paddingLeft: 50 }}>
+        <TextField id="outlined-basic" label="Caption 3" variant="outlined" />
+      </div>
+
+      <div className="box">
+        <h2>Title</h2>
+        {savedNotes.map((n) => (
+          <p key={n}>{n}</p>
+        ))}
+      </div>
+      <div className="box">
+        <h2>Caption 1</h2>
+        {savedNotes.map((n) => (
+          <p key={n}>{n}</p>
+        ))}
+      </div>
+      <div className="box">
+        <h2>Caption 2</h2>
+        {savedNotes.map((n) => (
+          <p key={n}>{n}</p>
+        ))}
+      </div>
+      <div className="box">
+        <h2>Caption 3</h2>
+        {savedNotes.map((n) => (
+          <p key={n}>{n}</p>
+        ))}
       </div>
     </>
   );
