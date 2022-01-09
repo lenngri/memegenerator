7a83527519487fd9bb5ff889dd93843ec4425e18
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-01-09 19:20:07 +0100

    Refactored imgflip template selection out of editor

---
+++ frontend/src/components/imgflipSelector.js
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Box } from '@mui/system';
+import { Container, ImageList, ImageListItem, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { useStoreState, useStoreActions } from 'easy-peasy';
+
+export default function ImgflipSelector() {
+  const setTemplate = useStoreActions((actions) => actions.setTemplate);
+  const imgflipTemplates = useStoreState((state) => state.imgflipTemplates);
+  const [open, setOpen] = React.useState(false);
+  const scroll = 'paper';
+
+  const handleClickOpen = () => () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Button variant="contained" onClick={handleClickOpen('paper')} sx={{ my: 5 }}>
+        Select Template
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        maxWidth={'xl'}
+        fullWidth={true}
+        // fullScreen={true}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">
+          Select a template from Imgflip.
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <Container sx={{ justifyContent: 'center', display: 'flex' }}>
+            <Box>
+              <ImageList style={{ cursor: 'pointer' }} variant="masonry" cols={3} gap={8}>
+                {imgflipTemplates.map((item) => (
+                  <ImageListItem key={item.id}>
+                    <img
+                      src={item.url}
+                      alt={item.name}
+                      onClick={(e) => {
+                        setTemplate(e.target);
+                        handleClose();
+                      }}
+                      loading="lazy"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            </Box>
+          </Container>
+        </DialogContent>
+        <DialogActions>
+          <Button color={'error'} onClick={handleClose}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- frontend/src/components/screens/EditorScreen.js
+++ frontend/src/components/screens/EditorScreen.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import NavBar from '../NavBar';
 import Editor from '../Editor';
-import { Box } from '@mui/system';
-import { Container, ImageList, ImageListItem } from '@mui/material';
-import { useStoreState, useStoreActions } from 'easy-peasy';
+import ImgflipSelector from '../imgflipSelector';
+import { Container } from '@mui/material';
 
 const EditorScreen = ({ logout }) => {
-  const setTemplate = useStoreActions((actions) => actions.setTemplate);
-  const imgflipTemplates = useStoreState((state) => state.imgflipTemplates);
-
   return (
     <div style={{ overflow: 'hidden' }}>
       <NavBar logout={logout} />
       <Editor />
-
       <Container sx={{ justifyContent: 'center', display: 'flex' }}>
-        <Box>
-          <ImageList variant="masonry" cols={3} gap={8}>
-            {imgflipTemplates.map((item) => (
-              <ImageListItem key={item.id}>
-                <img
-                  src={item.url}
-                  alt={item.name}
-                  onClick={(e) => setTemplate(e.target)}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </Box>
+        <ImgflipSelector />
       </Container>
     </div>
   );
