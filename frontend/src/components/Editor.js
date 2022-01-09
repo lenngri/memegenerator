fa54c481ce0d08f9a85e63b6f72ceed76355165f
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-01-09 12:20:40 +0100

    Select imgflip template from list

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -2,38 +2,36 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useStoreState } from 'easy-peasy';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Stack } from '@mui/material';
 
 // const C_WITDH = 500;
 // const MARGIN = 40;
 // const C_HEIGHT = 400 + MARGIN;
 
-const Editor = () => {
+const Editor = ({ templateUrl }) => {
   // Source Editor Canvas: https://www.youtube.com/watch?v=-AwG8yF06Po
   const canvas = useRef(null);
   const [memeIndex, setMemeIndex] = useState(0);
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
-  const [memeToEdit, setMemeToEdit] = useState(null);
-  const imgflipTemplates = useStoreState((state) => state.imgflipTemplates);
+  const [template, setTemplate] = useState(null);
+  console.log(templateUrl);
 
   useEffect(() => {
-    if (imgflipTemplates.length) {
-      const memeToEdit = new Image();
-      memeToEdit.src = imgflipTemplates[memeIndex].url;
-      memeToEdit.onload = () => setMemeToEdit(memeToEdit);
-    }
-  }, [imgflipTemplates, memeIndex]);
+    const template = new Image();
+    template.src = templateUrl;
+    template.onload = () => setTemplate(template);
+  }, [templateUrl]);
 
   useEffect(() => {
-    if (memeToEdit && canvas) {
+    if (template && canvas) {
       const scaling = 0.5;
-      const editWidth = memeToEdit.naturalWidth * scaling;
-      const editHeight = memeToEdit.naturalHeight * scaling;
+      const editWidth = template.naturalWidth * scaling;
+      const editHeight = template.naturalHeight * scaling;
       const ctx = canvas.current.getContext('2d');
       // ctx.fillStyle = 'black';
-      // ctx.fillRect(0, 0, memeToEdit.naturalWidth, memeToEdit.naturalHeight);
-      ctx.drawImage(memeToEdit, 0, 0, editWidth, editHeight);
+      // ctx.fillRect(0, 0, template.naturalWidth, template.naturalHeight);
+      ctx.drawImage(template, 0, 0, editWidth, editHeight);
 
       ctx.font = '32px Comic Sans MS';
       ctx.fillStyle = 'black';
@@ -42,7 +40,7 @@ const Editor = () => {
       ctx.fillText(topText, editWidth / 2, editHeight * 0.1);
       ctx.fillText(bottomText, editWidth / 2, editHeight * 0.9);
     }
-  }, [memeToEdit, canvas, topText, bottomText]);
+  }, [template, canvas, topText, bottomText]);
 
   // Source: https://developer.mozilla.org/de/docs/Web/API/HTMLCanvasElement/toDataURL (08.01.2021)
   const downloadMeme = () => {
@@ -64,51 +62,51 @@ const Editor = () => {
             justifyContent: 'center',
           }}
         >
-          {memeToEdit ? (
+          {template ? (
             <canvas
               id="canvas"
               ref={canvas}
-              width={memeToEdit.naturalWidth * 0.5}
-              height={memeToEdit.naturalHeight * 0.5}
+              width={template.naturalWidth * 0.5}
+              height={template.naturalHeight * 0.5}
             ></canvas>
           ) : (
             <></>
           )}
-          <TextField
-            required
-            id="outlined-required"
-            label="Caption"
-            onChange={(e) => setTopText(e.target.value)}
-            sx={{ mt: 2, mb: 2 }}
-          />
-          <TextField
-            required
-            id="outlined-required"
-            label="Caption"
-            onChange={(e) => setBottomText(e.target.value)}
-            sx={{ mt: 2, mb: 2 }}
-          />
-          <Button
-            variant="contained"
-            sx={{ mb: 2 }}
-            onClick={() => {
-              setMemeIndex(memeIndex + 1);
-            }}
-          >
-            Next
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ mb: 2 }}
-            onClick={() => {
-              setMemeIndex(memeIndex - 1);
-            }}
-          >
-            Previous
-          </Button>
-          <Button variant="contained" sx={{ mb: 2 }} onClick={downloadMeme}>
-            Download
-          </Button>
+          <Stack direction="row" spacing={1} sx={{ mt: 2, mb: 2 }}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Top Caption"
+              onChange={(e) => setTopText(e.target.value)}
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Bottom Caption"
+              onChange={(e) => setBottomText(e.target.value)}
+            />
+          </Stack>
+          <Stack direction="row" spacing={1} sx={{ mt: 2, mb: 2 }}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setMemeIndex(memeIndex + 1);
+              }}
+            >
+              Next
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setMemeIndex(memeIndex - 1);
+              }}
+            >
+              Previous
+            </Button>
+            <Button variant="contained" sx={{ mb: 2 }} onClick={downloadMeme}>
+              Download
+            </Button>
+          </Stack>
         </Box>
       </Container>
     </div>
--- frontend/src/components/screens/EditorScreen.js
+++ frontend/src/components/screens/EditorScreen.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../NavBar';
 import Editor from '../Editor';
+import { Box } from '@mui/system';
+import { Container, ImageList, ImageListItem } from '@mui/material';
+import { useStoreState } from 'easy-peasy';
 
 const EditorScreen = ({ logout }) => {
+  const [templateUrl, setTemplateUrl] = useState(null);
+  const imgflipTemplates = useStoreState((state) => state.imgflipTemplates);
+
   return (
     <div style={{ overflow: 'hidden' }}>
       <NavBar logout={logout} />
-      <Editor />
+      <Editor templateUrl={templateUrl} />
+      <Container sx={{ justifyContent: 'center', display: 'flex' }}>
+        <Box>
+          <ImageList variant="masonry" cols={3} gap={8}>
+            {imgflipTemplates.map((item) => (
+              <ImageListItem key={item.id}>
+                <img
+                  src={item.url}
+                  alt={item.name}
+                  onClick={(e) => setTemplateUrl(e.target.src)}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Box>
+      </Container>
     </div>
   );
 };
