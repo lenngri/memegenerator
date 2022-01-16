ccd14e7fdf16c5288bc913c05b42b3ece0e95d11
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-01-16 12:52:21 +0100

    fixes dependencies & node version in backend

--- backend/controller/template.controller.js
+++ backend/controller/template.controller.js
@@ -20,16 +20,5 @@ exports.uploadSingle = async function(req, res, next) {
     }
 }
 
-const fileSizeFormatter = (bytes, decimal = 2) => {
-    if (bytes === 0) {
-        return '0 bytes'
-    } else {
-        const dm = decimal;
-        const sizes = ['Bytes', 'KB', 'MB', 'GB', ];
-        const index = Math.floor(Math.log(bytes) / Math.log(1000));
-        return parseFloat((bytes / Math.pow(1000, index)).toFixed(dm)) + '-' + sizes[index]
-    }
-}
-
 
 
