d1cf3d5e63f0271635709df35db76e8977b75632
illegal-riegel <manu.riegel@googlemail.com>
2022-01-16 12:00:51 +0100

    dieser commit enthält alle basicauth sachen von mi

--- backend/database/models/user.js
+++ backend/database/models/user.js
@@ -1,65 +1,62 @@
-//Database model to save Users to the MongoDB Database
-
+const crypto = require("crypto");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, 'Please choose a username. A cool one.']
+        required: [true, "Please provide a username"],
     },
-
-//this article explains the email verification "/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/," https://stackoverflow.com/questions/15017052/understanding-email-validation-using-javascript
     email: {
         type: String,
-        required: [true, 'You forgot to add an email address.'],
-        unique: [true, 'This email address is already taken.'],
-        match: [/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/, 'This is not a valid email format.'],
+        required: [true, "Please provide an email address"],
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email address",
+        ],
     },
-
     password: {
         type: String,
-        required: [true, 'Please select a password'],
-        minlength: [8, 'Your password must be 8 characters long. You have {VALUE}'],
+        required: [true, "Please add a password"],
+        minlength: 6,
         select: false,
     },
-    // resetPasswordToken: String,
-    // resetPasswordExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
 });
 
-//If the password is not remodified it is not rehashed
-// UserSchema.pre("save", async function (next) {
-//     if (!this.isModified("password")) {
-//         next();
-//     }
-//     //Salt is a bcrypt function that takes the password and generates a 10-digit hash
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-// });
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
 
-//The entered password is compoared with the password saved for the user in the database
-// UserSchema.methods.matchPasswords = async function (password) {
-//     return await bcrypt.compare(password, this.password);
-// };
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
-// UserSchema.methods.getResetPasswordToken = function () {
-//     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//         expiresIn: process.env.JWT_EXPIRE,
-//     });
-// };
+UserSchema.methods.matchPasswords = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-// //Timespan for password reset is limited to 5min
-// UserSchema.methods.getResetPasswordToken = function () {
-//     const resetToken = crypto.randomBytes(20).toString("hex");
+UserSchema.methods.getSignedToken = function() {
+    return jwt.sign({ id: this._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+};
 
-//     this.resetPasswordtoken = crypto
-//         .createHash("sha256")
-//         .update(resetToken)
-//         .digest("hex");
-//     this.resetPasswordExpirt = Date.now () + 5 * (60 * 1000);
+UserSchema.methods.getResetPasswordToken = function() {
+    const resetToken = crypto.randomBytes(20).toString("hex");
 
-//     return resetToken;
-// };
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
+
+    return resetToken;
+};
 
 const User = mongoose.model("User", UserSchema);
 
