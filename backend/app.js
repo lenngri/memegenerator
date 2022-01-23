16f0d6df74843d82e58f09a6ad48e7f7c55d604e
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-01-23 17:10:56 +0100

    Add cors to app.js (+ some spaces and ; because of my formatter)

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 const userRouter = require('./routes/user.routes');
@@ -16,18 +16,20 @@ const privateRouter = require('./routes/private.routes');
 const mongoDB = require('./database/connection');
 
 const app = express();
+app.use(cors());
 
 // view engine setup
 app.set('view engine', 'pug');
 
-
 // https://attacomsian.com/blog/uploading-files-nodejs-express
-app.use(fileUpload({
-  createParentPath: true,
-  limits: { fileSize: 50 * 1024 * 1024 },
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,21 +37,21 @@ app.use(cookieParser());
 
 // handles static routes and serves react frontend
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // sets up api routes
 app.use('/api/user', userRouter);
 app.use('/api/template', templateRouter);
 app.use('/api/private', privateRouter);
-app.use('/api/meme', memeRouter)
+app.use('/api/meme', memeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
