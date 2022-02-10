da311d8200083f8c7ad445b0eb3459be5d5be41c
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-02-10 17:50:24 +0100

    changes URI helper to not use regex

--- backend/helpers/uriParser.helper.js
+++ backend/helpers/uriParser.helper.js
@@ -4,15 +4,15 @@
 exports.parseURI = function(URI) {
 
     const regex = /^data:.+\/(.+);base64,(.*)$/;
-    
+
     console.log("extracting file extension using uriParser.helper")
-    const extension = URI.match(regex)[1];
+    const extension = URI.split(',')[0].split(':')[1].split(';')[0];
     
     console.log("extracting data using uriParser.helper")
-    const data = URI.match(regex)[2];
+    const data = URI.split(',')[1];
 
     console.log("parsing data to buffer using uriParser.helper")
-    const buffer = Buffer.from(data, 'base64')
+    const buffer = Buffer.from(data, 'base64');
 
     return {image: buffer, extension: extension}
 }
