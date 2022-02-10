f712a9a66ce39eaf0641a7d9da4ca03e4a779cab
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-02-10 17:11:48 +0100

    use naturalWidth to save all of the image

--- frontend/src/components/generateTemplateObject.js
+++ frontend/src/components/generateTemplateObject.js
@@ -15,8 +15,8 @@ function getDataUrl(img) {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   // Set width and height
-  canvas.width = img.width;
-  canvas.height = img.height;
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
   // Draw the image
   ctx.drawImage(img, 0, 0);
   return canvas.toDataURL('image/jpeg');
