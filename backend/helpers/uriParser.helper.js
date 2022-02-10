0682ec8a67ea8631ba83c1e8e41f0bf92823fee3
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-02-10 17:34:21 +0100

    adds console logs to uriParser.helper

--- backend/helpers/uriParser.helper.js
+++ backend/helpers/uriParser.helper.js
@@ -2,11 +2,16 @@
 // source: https://www.edureka.co/community/95996/how-do-i-parse-a-data-url-in-node
 
 exports.parseURI = function(URI) {
-    console.log("parsing dataURI to using uriParser.helper")
+
     const regex = /^data:.+\/(.+);base64,(.*)$/;
+    
+    console.log("extracting file extension using uriParser.helper")
     const extension = URI.match(regex)[1];
+    
+    console.log("extracting data using uriParser.helper")
     const data = URI.match(regex)[2];
 
+    console.log("parsing data to buffer using uriParser.helper")
     const buffer = Buffer.from(data, 'base64')
 
     return {image: buffer, extension: extension}
