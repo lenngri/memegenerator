519b7497491ce94479c59a072ab04fb57da2aeab
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-02-11 18:28:27 +0100

    finishes createSingle route for memes

--- backend/helpers/konvaParser.helper.js
+++ backend/helpers/konvaParser.helper.js
@@ -1,20 +1,17 @@
-exports.memeFromKonvaObject = (konvaObject, backgroundImage) => {
+exports.memeFromKonvaObject = (konvaObject, filePath) => {
   // create stage (konva canvas object) from konvaObject
   const stage = Konva.Node.create(konvaObject);
-
-  // create Konva Image instance from background image
-  const background = new Konva.Image({
-    image: backgroundImage,
-    width: stage.width(),
-    height: stage.height(),
-  });
+  
   // fetch layer to add background image to
   const layer = stage.getLayers()[0];
 
-  // append the background image to the layer
-  layer.add(background);
-
-  // move the background image to the background
-  background.moveToBottom();
-  return stage.toDataURL();
+  const background = Konva.Image.fromURL(filePath, function(image){
+    // image is Konva.Image instance
+    // append the background image to the layer
+    layer.add(image);
+    // move the background image to the background
+    image.moveToBottom();
+  });
+  
+  return stage.toDataURL({ mimeType: 'image/jpeg' });
 };
--- backend/service/meme/createSingle.meme.service.js
+++ backend/service/meme/createSingle.meme.service.js
@@ -1,25 +1,75 @@
-const { memeFromKonvaObject } = require('../../helpers/konvaParser.helper');
-const Template = require('../../database/models/template.model')
-const { join } = require('path')
+const { memeFromKonvaObject } = require("../../helpers/konvaParser.helper");
+const { fileSizeFormatter } = require("../../helpers/fileSizeFormatter.helper")
+const { parseURI } = require("../../helpers/uriParser.helper");
+const { writeFile } = require("../../helpers/fileSaver.helper")
+const Template = require("../../database/models/template.model");
+const Meme = require("../../database/models/meme.model")
+const { join } = require("path");
 
+exports.createSingleMemeService = async function (req, res) {
+  console.log("running create single meme route");
 
-exports.createSingleMemeService = async function(req, res) {
+  try {
+    if (!req.body.konva) {
+      res.send({
+        status: false,
+        message: "No file uploaded!",
+      });
+    } else {
+      // retrieve template from database
+      const template = await Template.findOne({ _id: req.body.templateID });
 
-    console.log("running create single meme route")
+      // create new meme from konva object
+      const dataURI = memeFromKonvaObject(req.body.konva, template.filePath);
+      if (!dataURI) console.log("failed to create meme in konvaParser.helper");
 
+      console.log("constructing upload object");
 
-    try {
+      console.log("writing buffer to file");
+      const data = parseURI(dataURI);
+      const fileName = Date.now().toString();
 
-        const template = await Template.findOne({_id: req.body.templateID})
-        const filePath = join(__dirname, template.filePath)
+      const file = {
+        name: fileName,
+        mimetype: data.extension,
+        path: `./uploads/meme/${req.body.userID}/${fileName}.${
+          data.extension.split("/")[1]
+        }`,
+        size: fileSizeFormatter(data.image.toString("base64").length),
+      };
 
-    } catch (error) {
+      const meme = new Meme({
+        userID: req.body.userID,
+        templateID: req.body.templateID,
+        title: req.body.title,
+        description: req.body.description,
+        memeCaptions: req.body.memeCaptions,
+        fileName: file.name,
+        fileType: file.mimetype,
+        filePath: file.path,
+        fileSize: file.size,
+        konva: req.body.konva,
+        isPrivate: req.body.isPrivate,
+        isHidden: req.body.isHidden,
+        isDraft: req.body.isDraft,
+        likes: req.body.likes,
+        comments: req.body,
+      });
 
-        console.log(error)
-
+      await meme.save(function (error, meme) {
+        if (error) console.log(error.message);
+        const proxyHost = req.headers["x-forwarded-host"];
+        const host = proxyHost ? proxyHost : req.headers.host;
+        const stableURL = "http://" + host + "/overview/" + meme._id;
+        res.status(200).json({
+          meme: meme,
+          stableURL: stableURL,
+        });
+        writeFile(join(__dirname, "../../", meme.filePath), data.image);
+        console.log("saved meme with ID: " + meme.id + " at " + meme.filePath);
+      });
     }
-
-
-    const meme = memeFromKonvaObject(newMeme.konva, filePath) 
-    if(meme) console.log("theres a meme!")
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
