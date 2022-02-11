836ffdb382045dbc406b3bde6fbe0c2722ac6140
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-02-11 18:12:15 +0100

    improved get captions function

--- frontend/src/components/MemeUpload.js
+++ frontend/src/components/MemeUpload.js
@@ -37,10 +37,13 @@ const MemeUpload = () => {
 
   const getCaptions = (konvaObject) => {
     const layer0Children = konvaObject.children[0].children;
+    console.log(layer0Children);
     const captionsArray = [];
-    for (let i = 1; i < layer0Children.length; i++) {
-      if (layer0Children[i].attrs.text === undefined) captionsArray.push('');
-      else captionsArray.push(layer0Children[i].attrs.text);
+    for (let i = 0; i < layer0Children.length; i++) {
+      if (layer0Children[i].className === 'Text') {
+        if (layer0Children[i].attrs.text === undefined) captionsArray.push('');
+        else captionsArray.push(layer0Children[i].attrs.text);
+      }
     }
     return captionsArray;
   };
