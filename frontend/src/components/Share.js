bb56845d41aaeb0096c5721641a75c506797af5f
Lennart Grigoleit <lennartgrigoleit@yahoo.de>
2022-02-12 13:35:16 +0100

    Feedback for sharing

--- frontend/src/components/Share.js
+++ frontend/src/components/Share.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStoreState } from 'easy-peasy';
 import { useClipboard } from 'use-clipboard-copy';
 import { Button } from '@mui/material';
+import { Snackbar, Alert } from '@mui/material';
 
 const Share = () => {
   const clipboard = useClipboard();
   const { memeObject } = useStoreState((state) => state.editor);
+  const [success, setSuccess] = useState(false);
 
   const handleShare = () => {
     clipboard.copy(window.location.origin + '/overview/' + memeObject._id);
+    setSuccess(true);
+  };
+
+  const handleSnackClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSuccess(false);
   };
 
   return (
@@ -16,6 +27,16 @@ const Share = () => {
       <Button disabled={!memeObject ? true : false} variant='contained' onClick={handleShare}>
         Share
       </Button>
+      <Snackbar
+        open={success}
+        autoHideDuration={4000}
+        onClose={handleSnackClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleSnackClose} severity='success' sx={{ width: '100%' }}>
+          Link to share meme copied!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
