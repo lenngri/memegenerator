c09c127070c1d8ab253de9883659bb73d39d296a
Hendrik Geiger <g9a2hvk9@gmail.com>
2022-02-13 22:14:27 +0100

    adds last minute fixes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,18 +4,12 @@ const router = express.Router();
 const {
     register,
     login,
-    forgotpassword,
-    resetpassword,
     external
 } = require('../controller/user.controller');
 
 router.route("/register").post(register);
 
 router.route("/login").post(login);
-
-router.route("/forgotpassword").post(forgotpassword);
-
-router.route("/resetpassword/:resetToken").put(resetpassword);
 
 router.route("/external/register").post(external)
 
